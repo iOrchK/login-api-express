e901fd9afa415a44f022b0d6ec751d73d7f17e02
Jorge Chable <yahicimosclick9193@gmail.com>
2020-09-21 09:41:05 -0500

    Definición de middleware para validar el token en el header Authorization

--- core/middlewares/authenticated.js
+++ core/middlewares/authenticated.js
@@ -1,15 +1,15 @@
-var jwt = require("jwt-simple");
-var moment = require("moment");
-var secret = process.env.SECRET;
+const jwt = require("jwt-simple");
+const moment = require("moment");
 
 exports.ensureAuth = function (req, res, next) {
   if (!req.headers.authorization) {
     console.log("No se encontró la cabezera de autorización");
     return res.status(403).send({ message: "Forbiden." });
   } else {
-    var token = req.headers.authorization.replace(/['"]+/g, "");
+    const secret = process.env.SECRET;
+    const token = req.headers.authorization.replace(/['"]+/g, "");
     try {
-      var payload = jwt.decode(token, secret);
+      const payload = jwt.decode(token, secret);
       if (payload.exp > moment().unix()) {
         return res.status(401).send({
           message: "Token expired",
